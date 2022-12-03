0deaff1304e1df2059084dcf64f29575b1cf247e
Ignacio Castro <ignaciocastro347@gmail.com>
2022-12-03 20:54:50 -0300

    0. Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,23 +8,21 @@ if __name__ == "__main__":
 
     r = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}".format(
-            argv[1]
-    ))
+            argv[1]))
     user = r.json()
 
     r = requests.get(
         "https://jsonplaceholder.typicode.com/users/{}/todos".format(
-            argv[1]
-    ))
+            argv[1]))
     todos = r.json()
 
-    amount_done_todos = len(list(filter(
-        lambda t: t["completed"], todos)))
+    done_todos = list(filter(
+        lambda t: t["completed"], todos))
 
     print("Employee {} is done with tasks({}/{}):".format(
         user["name"],
-        amount_done_todos,
+        len(done_todos),
         len(todos)
     ))
-    for todo in todos:
+    for todo in done_todos:
         print("\t {}".format(todo["title"]))
