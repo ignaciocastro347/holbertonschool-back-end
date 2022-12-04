8ae39475aa79e46db7a52c6845fb2e24b83c09e7
Ignacio Castro <ignaciocastro347@gmail.com>
2022-12-04 14:21:56 -0300

    2. Export to JSON

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Console Module """
+
+
+if __name__ == "__main__":
+    from json import dump
+    import requests
+    from sys import argv
+
+    r = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(
+            argv[1]))
+    user = r.json()
+
+    r = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+            argv[1]))
+    todos = r.json()
+
+    todos = list(map(lambda t: {
+        "task": t["title"],
+        "completed": t["completed"],
+        "username": user["username"]
+    }, todos))
+
+    with open(str(argv[1]) + '.json', 'w') as file:
+        dump({ "{}".format(argv[1]): todos }, file)
+						
