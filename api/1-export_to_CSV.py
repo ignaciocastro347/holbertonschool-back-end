00b5ed63cf175442397022ed32d84eeb163c4066
Ignacio Castro <ignaciocastro347@gmail.com>
2022-12-04 14:03:26 -0300

    1. Export to CSV

---
+++ api/1-export_to_CSV.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Console Module """
+
+
+if __name__ == "__main__":
+    import csv
+    import requests
+    from sys import argv
+
+    r = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(
+            argv[1]))
+    user = r.json()
+
+    r = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+            argv[1]))
+    todos = r.json()
+
+    with open(argv[1] + '.csv', 'w') as file:
+        for todo in todos:
+            writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+            writer.writerow([todo['userId'],
+                             user["name"],
+                             todo['completed'],
+                             todo['title']])
